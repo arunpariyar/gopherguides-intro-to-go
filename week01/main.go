08d916b3079b3d84a6fa1b13368a3a7196db9214
Arun Pariyar <arun.pariyar@gmail.com>
2021-09-29 11:33:38 +0200

    add use of fmt package to print values

--- week01/main.go
+++ week01/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
+	//printing hello world
 	fmt.Println("Hello, World!")
+	
+	//using printf and correct print verbs to print the respective values
+	fmt.Printf("Printing, %s, %d, %t \n", string("Go"), int(42), bool(true))
 }
