7aa647de42dd805ff76e57ddd40396cf5c4f68ab
Arun Pariyar <arun.pariyar@gmail.com>
2021-09-30 13:02:36 +0200

    add seperate the Printf statement for each value

--- week01/main.go
+++ week01/main.go
@@ -7,5 +7,7 @@ func main(){
 	fmt.Println("Hello, World!")
 	
 	//using printf and correct print verbs to print the respective values
-	fmt.Printf("Printing, %s, %d, %t \n", string("Go"), int(42), bool(true))
+	fmt.Printf("Printing, %s \n", string("Go"))
+	fmt.Printf("Printing, %d \n", int(42))
+	fmt.Printf("Printing, %t \n", bool(true))
 }
