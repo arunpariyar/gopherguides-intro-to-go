9523799f0ca206431cf51955734b43644eb27bf4
Arun Pariyar <arun.pariyar@gmail.com>
2021-10-01 16:34:21 +0200

    add use of variables before printing

--- week01/main.go
+++ week01/main.go
@@ -6,8 +6,14 @@ func main(){
 	//printing hello world
 	fmt.Println("Hello, World!")
 	
+	//various variable declartion and initialization
+	var language string	// variable declaration without initialization
+	num := uint8(42) 	// short declartion with desired data type
+	isTrue := true 		// short declaration
+	language = "Go" 	// initialization
+
 	//using printf and correct print verbs to print the respective values
-	fmt.Printf("Printing, %s \n", "Go")
-	fmt.Printf("Printing, %d \n", 42)
-	fmt.Printf("Printing, %t \n", true)
+	fmt.Printf("Printing, %s \n", language)
+	fmt.Printf("Printing, %d \n", num )
+	fmt.Printf("Printing, %t \n", isTrue)
 }
