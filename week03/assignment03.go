0eb507b595005ad0a0a1c29d78e2c8bb924f5d04
Arun Pariyar <arun.pariyar@gmail.com>
2021-10-16 12:52:22 +0200

    add assignment ready for submission

--- week03/assignment03.go
+++ week03/assignment03.go
@@ -1,75 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Movie struct {
-	Name string
-	Length int 
-	rating float64
-	plays int
-	viewers int 
+	Name    string
+	Length  int
+	rating  float32
+	plays   int
+	viewers int
+	critics []float32
 }
-
-type Critiquefn func( *Movie)(float32, error)
 
 type Theatre struct {
-
+	name  string
 }
+
+type Critiquefn func(*Movie) (float32, error)
 
 func (m *Movie) Rate(rating float32) error {
-
-	//Calling Rate should track this rating. If the number of plays is 0 return the following error: fmt.Errorf("can't review a movie without watching it first")
-
 	if m.plays == 0 {
 		return fmt.Errorf("can't review a movie without watching it first")
-	}else{ 
-		return nil
 	}
-
+	if rating == 0 {
+		return fmt.Errorf("rating can't be a zero value")
+	}
+	m.rating = rating
+	return nil
 }
 
-func (m *Movie) Play(viewers int){
-	//Calling Play should increase both the number of viewers, as well as the number of plays, for the movie.
+func (m *Movie) Play(viewers int) {
 	m.viewers += viewers
-	m.plays += viewers
+	m.plays++
 }
 
 func (m Movie) Viewers() int {
-	//returns the number (int) of people who have viewed the movie.
 	return m.viewers
 }
 
-func (m Movie) Rating() float64 {
-	//Rating takes no arguments and returns the rating (float64) of the movie. This can be calculated by the total ratings for the movie divided by the number of times the movie has been played.
-	rating := m.rating / float64(m.plays)
+func (m Movie) Plays() int {
+	return m.plays
+}
 
+func (m Movie) Rating() float64 {
+	rating := float64(m.rating) / float64(m.plays)
 	return rating
 }
 
 func (m Movie) String() string {
-  // String should return a string that that includes the name, length, and rating of the film. Ex. Wizard of Oz (102m) 99.0%	
-  return fmt.Sprintf("%s (%dm) %.1f", m.Name, m.Length, m.rating)
+	return fmt.Sprintf("%s (%dm) %.1f%%", m.Name, m.Length, m.rating)
 }
 
+func (t *Theatre) TPlay(viewers int, movies ...*Movie) error {
+	if(viewers) == 0 {
+		return fmt.Errorf("viewers cant be zero")
+	}
+	if len(movies) == 0 {
+		return fmt.Errorf("no movies to play")
+	}
+	for _, m := range movies {
+		m.Play(viewers)
+	}
+	return nil
+}
 
-/* 
-type Movie struct {
-	Name string
-	Length int 
-	rating float64
-	plays int
-	viewers int 
-*/
+func (t Theatre) Critique(m []*Movie, cfn Critiquefn) error {
+	if len(m) == 0 {
+		return fmt.Errorf("no movies to play")
+	}
+	for _, m := range m {
+		m.Play(1)
+		rating, err := cfn(m)
+
+		if err != nil {
+			return err
+		}
+	
+		err = m.Rate(rating)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Critic(m *Movie) (float32, error) {
+	if m.plays == 1 {
+		return 0, fmt.Errorf("this is the films first critique play value expected more than 1 got %d", m.plays)
+	}
+
+	rating := float32(0)
+
+	for _, v := range m.critics {
+		rating += v
+	}
+
+	rating = rating / float32(len(m.critics)) * 10
+	return float32(rating), nil
+}
 
 func main() {
-	 m := Movie{
-		 Name : "Wizard of Oz",
-		 Length : 102,
-		 rating : 0,
-		 plays : 10000,
-		 viewers: 10000,
-	 }
 
-	fmt.Println(m.String())
-	
 }
---
+++ week03/assignment03_test.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Rate(t *testing.T) {
+
+	m := &Movie{
+		Name:    "Avatar",
+		Length:  16,
+		rating:  98.75,
+		plays:   4,
+		viewers: 400,
+	}
+
+	err := m.Rate(float32(m.rating))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Play(t *testing.T) {
+
+	m := &Movie{
+		Name:    "Avengers: Endgame",
+		Length:  181,
+		rating:  88.75,
+		plays:   3,
+		viewers: 300,
+		critics: []float32{9, 9, 9, 8.5},
+	}
+
+	//storing the value of play and viewer before adding new viewers
+	playsBefore := m.plays
+	viewersBefore := m.viewers
+
+	//new viewers for the movie that we want to add
+	newViewers := 100
+
+	//call the Play function with
+	m.Play(newViewers)
+
+	//storing new values of play and viewer after adding new viewers
+	playsAfter := m.plays
+	viewersAfter := m.viewers
+	// calculating the difference between the before and after adding new viewers
+	playsDiff := playsAfter - playsBefore
+	viewsDiff := viewersAfter - viewersBefore
+
+	if (playsDiff != 1) || (viewsDiff != newViewers) {
+		t.Errorf("expected plays 1 got %d expected viewers %d got %d \n", playsDiff, newViewers, viewsDiff)
+	}
+}
+
+func Test_Viewers(t *testing.T) {
+
+	m := Movie{
+		Name:    "Titanic",
+		Length:  195,
+		rating:  0,
+		plays:   2,
+		viewers: 200,
+	}
+	//storing the result of calling Viewers Method
+	result := m.Viewers()
+	//logging error if difference found
+	if result != m.viewers {
+		t.Errorf("expected %d got %d \n", m.viewers, result)
+	}
+}
+
+func Test_Plays(t *testing.T) {
+	m := Movie{
+		Name:    "Star Wars: The Force Awakens",
+		Length:  135,
+		rating:  0,
+		plays:   100,
+		viewers: 100,
+	}
+	//storing the result of calling the Plays Method
+	result := m.Plays()
+
+	//logging error if difference found
+	if result != m.plays {
+		t.Errorf("expected %d got %d \n", m.plays, result)
+	}
+}
+
+func Test_Rating(t *testing.T) {
+	m := Movie{
+		Name:    "Avatar",
+		Length:  16,
+		rating:  0,
+		plays:   4,
+		viewers: 400,
+	}
+	//calcuating rating
+	rating := float64(m.rating) / float64(m.plays)
+	//storing the result of calling Rating fuction
+	result := m.Rating()
+
+	if result != rating {
+		t.Errorf("expected %.2f got %.2f \n", rating, result)
+	}
+}
+
+func Test_String(t *testing.T) {
+	m := Movie{
+		Name:    "Avatar",
+		Length:  16,
+		rating:  0,
+		plays:   4,
+		viewers: 400,
+	}
+
+	expString := fmt.Sprintf("%s (%dm) %.1f%%", m.Name, m.Length, m.rating)
+
+	result := m.String()
+
+	if result != expString {
+		t.Errorf("expected %q got %q \n.", expString, result)
+	}
+}
+
+func Test_TPlay(t *testing.T) {
+
+	m1 := &Movie{
+		Name:    "Avatar",
+		Length:  16,
+		rating:  0,
+		plays:   4,
+		viewers: 400,
+		critics: []float32{10, 10, 10, 9.5},
+	}
+
+	m2 := &Movie{
+		Name:    "Avengers: Endgame",
+		Length:  181,
+		rating:  0,
+		plays:   3,
+		viewers: 300,
+		critics: []float32{9, 9, 9, 8.5},
+		
+	}
+
+	m3 := &Movie{
+		Name:    "Titanic",
+		Length:  195,
+		rating:  0,
+		plays:   2,
+		viewers: 200,
+		critics: []float32{8, 8, 8, 7.5},
+	}
+
+	m4 := &Movie{
+		Name:    "Star Wars: The Force Awakens",
+		Length:  135,
+		rating:  0,
+		plays:   100,
+		viewers: 100,
+		critics: []float32{7, 7, 7, 6.5},
+	}
+
+	t1 := Theatre{
+		name: "The Wall",
+	}
+
+	mlist := []*Movie{m1, m2, m3, m4}
+
+	err := t1.TPlay(100, mlist...)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func Test_Critique(t *testing.T){
+	m1 := &Movie{
+		Name:    "Avatar",
+		Length:  16,
+		rating:  0,
+		plays:   4,
+		viewers: 400,
+		critics: []float32{10, 10, 10, 9.5},
+	}
+
+	m2 := &Movie{
+		Name:    "Avengers: Endgame",
+		Length:  181,
+		rating:  0,
+		plays:   3,
+		viewers: 300,
+		critics: []float32{9, 9, 9, 8.5},
+	}
+
+	m3 := &Movie{
+		Name:    "Titanic",
+		Length:  195,
+		rating:  0,
+		plays:   2,
+		viewers: 200,
+		critics: []float32{8, 8, 8, 7.5},
+	}
+
+	m4 := &Movie{
+		Name:    "Star Wars: The Force Awakens",
+		Length:  135,
+		rating:  0,
+		plays:   1,
+		viewers: 100,
+		critics: []float32{7, 7, 7, 6.5},
+	}
+
+	t1 := Theatre{
+		name : "The Wall",
+	}
+
+	mlist := []*Movie{m1,m2,m3,m4}
+
+	err :=  t1.Critique(mlist, Critic)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Critic(t *testing.T){
+	m4 := &Movie{
+			Name:    "Star Wars: The Force Awakens",
+			Length:  135,
+			rating:  0,
+			plays:   2,
+			viewers: 200,
+			critics: []float32{7, 7, 7, 6.5},
+		}	
+	
+	rating := float32(68.75) // hard calculated
+
+	result, err := Critic(m4)
+		if err != nil {
+			t.Error(err)
+		}
+	
+	if result != rating {
+		t.Errorf("Expected %f got %f \n", rating, result)
+	}
+	
+}
