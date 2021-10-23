ae74377a3f2b42037dc02042b7214f79f94ecfb3
Arun Pariyar <arun.pariyar@gmail.com>
2021-10-23 14:18:56 +0200

    add refactored code and ready for submission

--- week04/assignment04.go
+++ week04/assignment04.go
@@ -24,22 +24,25 @@ type Teardowner interface {
 }
 
 func (v *Venue) Entertain(aud int, artists ...Entertainer) error {
-
+	//check if audience is null
 	if aud == 0 {
 		return fmt.Errorf("audience cant be empty")
 	}
+	//check if artist is null
 	if len(artists) == 0 {
 		return fmt.Errorf("artist not entered")
 	}
+	//update the value of venues Audience with audience entered
 	v.Audience = aud
 
+	//The Venue should check each Entertainer to see if it implements the Setuper or Teardowner interfaces and call them accordingly.
 	for _, artist := range artists {
 		if st, ok := artist.(Setuper); ok {
 			if err := st.Setup(*v); err != nil {
 				return err
 			}
 		}
-		
+		//For each Entertainer call its Perform method passing in the Venue
 		if err := artist.Perform(*v); err != nil {
 			return err
 		}
@@ -50,6 +53,46 @@ func (v *Venue) Entertain(aud int, artists ...Entertainer) error {
 			}
 		}
 	}
+	return nil
+}
+
+type band struct {
+	name string
+}
+
+func (b band) Name() string {
+	return b.name
+}
+
+func (b band) Perform(v Venue) error {
+	name := b.Name()
+	fmt.Fprintf(v.Log, "%s has performed for %d people.\n", name, v.Audience)
+	return nil
+}
+
+func (b band) Teardown(v Venue) error {
+	name := b.Name()
+	fmt.Fprintf(v.Log, "%s has completed teardown.\n", name)
+	return nil
+}
+
+type comedian struct {
+	name string
+}
+
+func (c comedian) Name() string {
+	return c.name
+}
+
+func (c comedian) Setup(v Venue) error {
+	name := c.Name()
+	fmt.Fprintf(v.Log, "%s has completed setup.\n", name)
+	return nil
+}
+
+func (c comedian) Perform(v Venue) error {
+	name := c.Name()
+	fmt.Fprintf(v.Log, "%s has performed for %d people.\n", name, v.Audience)
 	return nil
 }
 
--- week04/assignment04_test.go
+++ week04/assignment04_test.go
@@ -6,35 +6,10 @@ import (
 	"testing"
 )
 
-type band struct {
-	name string
-}
+func Test_Entertain1(t *testing.T) {
 
-func (b band) Name() string {
-	return b.name
-}
-
-func (b band) Perform(v Venue) error {
-	name := b.Name()
-	fmt.Fprintf(v.Log, "%s has performed for %d people.\n", name, v.Audience)
-	return nil
-}
-
-func (b band) Setup(v Venue) error {
-	name := b.Name()
-	fmt.Fprintf(v.Log, "%s has completed setup.\n", name)
-	return nil
-}
-
-func (b band) Teardown(v Venue) error {
-	name := b.Name()
-	fmt.Fprintf(v.Log, "%s has completed teardown.\n", name)
-	return nil
-}
-
-func Test_Entertain(t *testing.T){
-	
 	res := &bytes.Buffer{}
+
 	v := Venue{
 		Audience: 0,
 		Log:      res,
@@ -44,14 +19,49 @@ func Test_Entertain(t *testing.T){
 		name: "Guns and Roses",
 	}
 
+	var _ Teardowner = b
+
 	err := v.Entertain(2, b)
-	if err != nil { 
+	if err != nil {
 		t.Error(err)
 	}
 
-	exp := fmt.Sprintf("Guns and Roses has completed setup.\nGuns and Roses has performed for %d people.\nGuns and Roses has completed teardown.\n",v.Audience)
+	exp := fmt.Sprintf("Guns and Roses has performed for %d people.\nGuns and Roses has completed teardown.\n", v.Audience)
 
 	if res.String() != exp {
 		t.Error("logs don't match")
 	}
+}
+
+func Test_Entertain2(t *testing.T) {
+
+	res := &bytes.Buffer{}
+
+	v := Venue{
+		Audience: 0,
+		Log:      res,
+	}
+
+	b := band{
+		name: "Guns and Roses",
+	}
+
+	c := comedian{
+		name: "Trevor Noah",
+	}
+
+	var _ Setuper = c
+	var _ Teardowner = b
+
+	err := v.Entertain(2, b, c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	exp := fmt.Sprintf("Guns and Roses has performed for %d people.\nGuns and Roses has completed teardown.\nTrevor Noah has completed setup.\nTrevor Noah has performed for %d people.\n", v.Audience, v.Audience)
+
+	if res.String() != exp {
+		t.Error("logs don't match")
+	}
+
 }
