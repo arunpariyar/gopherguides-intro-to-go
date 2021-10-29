34465d5f2e3739aef10972c0bfc0509e75f8d1ee
Arun Pariyar <arun.pariyar@gmail.com>
2021-10-29 19:50:31 +0200

    fix redo everything from scratch

--- week05/store_test.go
+++ week05/store_test.go
@@ -1,1 +1,31 @@
-package demo
+package demo 
+
+import "testing"
+import "reflect"
+
+func Test_db(t *testing.T){
+	
+	s := &Store{}
+
+	table := []struct{
+		name	string
+		store 	*Store
+		exp 	data 
+	}{
+		{name:"empty store", store:s, exp:s.data,},
+	}
+
+	for _, tt := range table{
+		t.Run(tt.name, func(t *testing.T){
+			data := tt.store.db()
+
+			if !reflect.DeepEqual(s.data, data){
+				t.Fatalf("%s expected %q got %q",tt.name, tt.exp, data)
+			}
+
+
+
+		})
+	}
+
+}
