5eb5a4fc645edc21fdca174dffd0688a4f0356a3
Arun Pariyar <arun.pariyar@gmail.com>
2021-11-02 17:34:17 +0100

    fix test file for store.go refactored, simplified and helper function add for comparing models

--- week05/store_test.go
+++ week05/store_test.go
@@ -1,51 +1,24 @@
 package demo
 
 import (
-	"reflect"
+	"fmt"
+	"sort"
 	"testing"
 )
 
-func Test_db(t *testing.T) {
+func Test_store_db(t *testing.T) {
 	t.Parallel()
-
-	s := &Store{}
-
-	table := []struct {
-		name  string
-		store *Store
-		exp   data
-	}{
-		{name: "empty store", store: s, exp: s.data},
+	s := &Store{
+		data: nil,
 	}
 
-	for _, tt := range table {
-		t.Run(tt.name, func(t *testing.T) {
-			data := tt.store.db()
-
-			if !reflect.DeepEqual(s.data, data) {
-				t.Fatalf("%s expected %q got %q", tt.name, tt.exp, data)
-			}
-		})
+	if len(s.data) != 0 {
+		t.Fatalf("expected 0 got %v", len(s.data))
 	}
 }
 
-func Test_All(t *testing.T) {
+func Test_Store_All(t *testing.T) {
 	t.Parallel()
-	tn1 := "users"
-	tn2 := "orders"
-
-	s1 := &Store{}
-	db1 := s1.db()
-	err1 := ErrTableNotFound{
-		table: "users",
-	}
-
-	s2 := &Store{
-		data: data{
-			"orders": {{"id": 1, "item": "desktop"}, {"id": 2, "item": "laptop"}},
-		},
-	}
-	db2 := s2.db()
 
 	table := []struct {
 		name  string
@@ -54,8 +27,34 @@ func Test_All(t *testing.T) {
 		mods  Models
 		exp   error
 	}{
-		{name: "empty store", store: s1, tn: tn1, mods: db1[tn1], exp: err1},
-		{name: "store with orders", store: s2, tn: tn2, mods: db2[tn2], exp: nil},
+		{name: "store with orders",
+			store: &Store{
+				data: data{
+					"orders": {
+						{"desktop": "dell"},
+						{"mobile": "apple"},
+						{"smartwatch": "xioami"},
+						{"gps": "garmin"},
+					},
+				},
+			},
+			tn: "orders",
+			mods: Models{
+				{"desktop": "dell"},
+				{"mobile": "apple"},
+				{"smartwatch": "xioami"},
+				{"gps": "garmin"},
+			},
+		},
+		{name: "empty store",
+			store: &Store{
+				data: data{
+					"orders": {},
+				},
+			},
+			tn:   "orders",
+			mods: Models{},
+		},
 	}
 
 	for _, tt := range table {
@@ -67,34 +66,18 @@ func Test_All(t *testing.T) {
 					t.Fatalf("%s expected error type %s got %T", tt.name, "ErrTableNotFound", err)
 				}
 
-				if !reflect.DeepEqual(err.Error(), tt.exp.Error()) {
+				if err.Error() != tt.exp.Error() {
 					t.Fatalf("%s expected %q got %q", tt.name, tt.exp.Error(), err.Error())
 				}
 			}
 
-			if !reflect.DeepEqual(tt.mods, mods) {
-				t.Fatalf("%s expected %q got %q", tt.name, tt.mods, mods)
-			}
+			assertSameModel(t, mods, tt.mods)
 		})
 	}
 }
 
-func Test_Len(t *testing.T) {
+func Test_Store_Len(t *testing.T) {
 	t.Parallel()
-	tn1 := "users"
-	tn2 := "orders"
-
-	s1 := &Store{}
-
-	s2 := &Store{
-		data: data{
-			"orders": {{"id": 1, "item": "desktop"}, {"id": 2, "item": "laptop"}},
-		},
-	}
-
-	err1 := ErrTableNotFound{
-		table: "users",
-	}
 
 	table := []struct {
 		name  string
@@ -103,21 +86,38 @@ func Test_Len(t *testing.T) {
 		len   int
 		exp   error
 	}{
-		{name: "empty store", store: s1, tn: tn1, len: 0, exp: err1},
-		{name: "store with orders", store: s2, tn: tn2, len: 2, exp: nil},
+		{
+			name:  "empty store",
+			store: &Store{},
+			tn:    "users",
+			len:   0,
+			exp: ErrTableNotFound{
+				table: "users",
+			},
+		},
+		{name: "store with orders",
+			store: &Store{
+				data: data{
+					"orders": {
+						{"id": 1, "item": "desktop"},
+						{"id": 2, "item": "laptop"},
+					},
+				},
+			},
+			tn:  "orders",
+			len: 2,
+			exp: nil},
 	}
 
 	for _, tt := range table {
 		t.Run(tt.name, func(t *testing.T) {
 			len, err := tt.store.Len(tt.tn)
-
-			// need to think if this block should be inside here
 			if err != nil {
 				if ok := IsErrTableNotFound(err); !ok {
 					t.Fatalf("%s expected error type %s got %T", tt.name, "ErrTableNotFound", err)
 				}
 
-				if !reflect.DeepEqual(err.Error(), tt.exp.Error()) {
+				if err.Error() != tt.exp.Error() {
 					t.Fatalf("%s expected %q got %q", tt.name, tt.exp.Error(), err.Error())
 				}
 			}
@@ -128,71 +128,46 @@ func Test_Len(t *testing.T) {
 	}
 }
 
-func Test_Insert(t *testing.T) {
-
-	tn := "orders"
-
-	s1 := &Store{
-		data: data{
-			"orders": {{"id": 1, "item": "desktop"}, {"id": 2, "item": "laptop"}},
-		},
-	}
-
-	m1 := Model{"id": 3, "item": "mobile"}
-
-	s2 := &Store{
-		data: data{
-			"orders": {{"id": 1, "item": "desktop"}, {"id": 2, "item": "laptop"}},
-		},
-	}
-	m2 := Model{"id": 3, "item": "mobile"}
-	m3 := Model{"id": 4, "item": "smartwatch"}
-
-	addOne := []Model{m1}
-	addTwo := []Model{m2, m3}
-
-	expMods1 := []Model{
-		{"id": 1, "item": "desktop"},
-		{"id": 2, "item": "laptop"},
-		{"id": 3, "item": "mobile"}}
-
-	expMods2 := []Model{
-		{"id": 1, "item": "desktop"},
-		{"id": 2, "item": "laptop"},
-		{"id": 3, "item": "mobile"},
-		{"id": 4, "item": "smartwatch"}}
+func Test_store_Insert(t *testing.T) {
+	t.Parallel()
 
 	table := []struct {
 		name  string
 		store *Store
 		tn    string
+		mlist []Model
 		mods  []Model
-		exp   []Model
 	}{
-		{name: "insert one item", store: s1, tn: tn, mods: addOne, exp: expMods1},
-		{name: "insert two item", store: s2, tn: tn, mods: addTwo, exp: expMods2},
-	}
-
-	for _, tt := range table {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.store.Insert(tt.tn, tt.mods...)
-			db := tt.store.db()
-			mods := db[tt.tn]
-			if reflect.DeepEqual(tt.exp, mods) {
-				t.Fatalf("%s expected models %v got %v", tt.name, tt.exp, mods)
-			}
-
-		})
-	}
-}
-
-func Test_Select(t *testing.T) {
-	tn := "orders"
-
-	// store with table and model
-	s1 := &Store{
-		data: data{
-			"orders": {
+		{
+			name: "insert one item",
+			store: &Store{
+				data: data{
+					"orders": {
+						{"desktop": "dell"},
+						{"mobile": "apple"},
+					},
+				},
+			},
+			tn:    "orders",
+			mlist: []Model{{"smartwatch": "xioami"}},
+			mods: []Model{
+				{"desktop": "dell"},
+				{"mobile": "apple"},
+				{"smartwatch": "xioami"},
+			}},
+		{
+			name: "insert two item",
+			store: &Store{
+				data: data{
+					"orders": {
+						{"desktop": "dell"},
+						{"mobile": "apple"},
+					},
+				},
+			},
+			tn:    "orders",
+			mlist: []Model{{"smartwatch": "xioami"}, {"gps": "garmin"}},
+			mods: []Model{
 				{"desktop": "dell"},
 				{"mobile": "apple"},
 				{"smartwatch": "xioami"},
@@ -200,82 +175,103 @@ func Test_Select(t *testing.T) {
 			},
 		},
 	}
-	c1 := Clauses{"gps": "garmin"}
-	len1 := len(c1)
-	m1 := Model{"gps": "garmin"}
 
-	//store with table but no model
-	s2 := &Store{
-		data: data{
-			"orders": {},
-		},
-	}
-	c2 := Clauses{"gps": "garmin"}
-	len2 := len(c2)
-	m2 := Model{}
-	err2 := &errNoRows{
-		clauses: c2,
-		table:   tn,
-	}
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			db := tt.store.db()
+			tt.store.Insert(tt.tn, tt.mlist...)
+			mods := db[tt.tn]
+			assertSameModel(t, mods, tt.mods)
 
-	//query with no clause
-	s3 := &Store{
-		data: data{
-			"orders": {
-				{"smartwatch": "xioami"},
-			},
-		},
+		})
 	}
-	c3 := Clauses{}
-	len3 := len(c3)
-	m3 := Model{"smartwatch": "xioami"}
-	err3 := &errNoRows{
-		clauses: c3,
-		table:   tn,
-	}
+}
 
-	//query with no table name
-	s4 := &Store{
-		data: data{
-			"orders": {
-				{"smartwatch": "xioami"},
-			},
-		},
-	}
-	c4 := Clauses{}
-	len4 := len(c3)
-	m4 := Model{"smartwatch": "xioami"}
-	err4 := ErrTableNotFound{
-		table: "",
-	}
-	//table test
+func Test_store_Select(t *testing.T) {
+	t.Parallel()
 	table := []struct {
 		name string
 		str  *Store
 		tn   string
 		cls  Clauses
-		mds  Model
+		mods Models
 		len  int
 		exp  error
 	}{
-		{name: "store with table and model", str: s1, tn: tn, cls: c1, mds: m1, len: len1, exp: nil},
-		{name: "store with table but no model", str: s2, tn: tn, cls: c2, mds: m2, len: len2, exp: err2},
-		{name: "query with no clause", str: s3, tn: tn, cls: c3, mds: m3, len: len3, exp: err3},
-		{name: "query with no table name", str: s4, tn: "", cls: c4, mds: m4, len: len4, exp: err4},
+		{
+			name: "store with table and model",
+			str: &Store{
+				data: data{
+					"orders": {
+						{"desktop": "dell"},
+						{"mobile": "apple"},
+						{"smartwatch": "xioami"},
+						{"gps": "garmin"},
+					},
+				},
+			},
+			tn:   "orders",
+			cls:  Clauses{"gps": "garmin"},
+			mods: []Model{{"gps": "garmin"}},
+			len:  1,
+			exp:  nil},
+		{
+			name: "store with table but no model",
+			str: &Store{
+				data: data{
+					"orders": {},
+				},
+			},
+			tn:   "orders",
+			cls:  Clauses{"gps": "garmin"},
+			mods: []Model{},
+			len:  1,
+			exp: &errNoRows{
+				clauses: Clauses{"gps": "garmin"},
+				table:   "orders",
+			},
+		},
+		{
+			name: "query with no clause",
+			str: &Store{
+				data: data{
+					"orders": {
+						{"smartwatch": "xioami"},
+					},
+				},
+			},
+			tn:   "orders",
+			cls:  Clauses{},
+			mods: []Model{{"smartwatch": "xioami"}},
+			len:  0,
+			exp: &errNoRows{
+				clauses: Clauses{},
+				table:   "orders",
+			}},
+		{
+			name: "query with no table name",
+			str: &Store{
+				data: data{
+					"orders": {
+						{"smartwatch": "xioami"},
+					},
+				},
+			},
+			tn:   "",
+			cls:  Clauses{},
+			mods: []Model{{"smartwatch": "xioami"}},
+			len:  0,
+			exp: ErrTableNotFound{
+				table: "",
+			}},
 	}
 
 	for _, tt := range table {
 		t.Run(tt.name, func(t *testing.T) {
 			mods, err := tt.str.Select(tt.tn, tt.cls)
 			if err != nil {
-				if !reflect.DeepEqual(err.Error(), tt.exp.Error()) {
+				if err.Error() != tt.exp.Error() {
 					t.Fatalf("%s expected %q got %q", tt.name, tt.exp.Error(), err.Error())
-				}
-
-				for _, mod := range mods {
-					if !reflect.DeepEqual(mod, tt.mds) {
-						t.Fatalf("%s expected %q got %q", tt.name, tt.mds, mod)
-					}
 				}
 			}
 
@@ -284,10 +280,50 @@ func Test_Select(t *testing.T) {
 			}
 
 			if len(mods) == 0 {
-				if !reflect.DeepEqual(err.Error(), tt.exp.Error()) {
-					t.Fatal("no models returned")
+				if err.Error() != tt.exp.Error() {
+					t.Fatalf("%s expected %q got %q", tt.name, tt.exp.Error(), err.Error())
 				}
 			}
+
+			assertSameModel(t, mods, tt.mods)
+
 		})
+	}
+}
+
+func assertSameModel(t testing.TB, act Models, exp Models) {
+	t.Helper()
+
+	k1 := []string{}
+	v1 := []string{}
+
+	k2 := []string{}
+	v2 := []string{}
+
+	for _, m := range act {
+		for k, v := range m {
+			k1 = append(k1, k)
+			vc1 := fmt.Sprintf("%s", v)
+			v1 = append(v1, vc1)
+		}
+	}
+
+	for _, m := range exp {
+		for k, v := range m {
+			k2 = append(k2, k)
+			vc2 := fmt.Sprintf("%s", v)
+			v2 = append(v2, vc2)
+		}
+	}
+
+	sort.Strings(k1)
+	sort.Strings(k2)
+	sort.Strings(v1)
+	sort.Strings(v2)
+
+	for i := 0; i < len(k1); i++ {
+		if k1[i] != k2[i] || v1[i] != v2[i] {
+			t.Fatal("models don't match")
+		}
 	}
 }
