26ee01308f52ffb2dfaefb11558880cecd6e789d
Arun Pariyar <arun.pariyar@gmail.com>
2021-12-01 11:37:34 +0100

    chore: basic file setup and go mod updated

---
+++ week10/errors.go
@@ -0,0 +1,51 @@
+package week09
+
+import "fmt"
+
+type ErrTitleInvalid string
+
+func (e ErrTitleInvalid) Error() string {
+	return fmt.Sprintf("draft must have a title, got %#v", string(e))
+}
+
+type ErrBodyInvalid string
+
+func (e ErrBodyInvalid) Error() string {
+	return fmt.Sprintf("draft must have a body, got %#v", string(e))
+}
+
+type ErrWriterInvalid string
+
+func (e ErrWriterInvalid) Error() string {
+	return fmt.Sprintf("draft must have a writer, got %#v", string(e))
+}
+
+type ErrStoryPublisherInvalid string
+
+func (e ErrStoryPublisherInvalid) Error() string {
+	return fmt.Sprintf("story must hava a publisher got %#v", string(e))
+}
+
+type ErrStoryCatagoryInvalid string
+
+func (e ErrStoryCatagoryInvalid) Error() string {
+	return fmt.Sprintf("story must hava a catagory got %#v", string(e))
+}
+
+type ErrSubscriberInvalidName string
+
+func (e ErrSubscriberInvalidName) Error() string {
+	return fmt.Sprintf("subscriber must hava a valid name got %#v", string(e))
+}
+
+type ErrSubscriberCatgoriesInvalid int
+
+func (e ErrSubscriberCatgoriesInvalid) Error() string {
+	return "subscriber must have at least one catagory"
+}
+
+type ErrSourcesEmpty int
+
+func (e ErrSourcesEmpty) Error() string {
+	return "news service sources is empty"
+}
---
+++ week10/story_test.go
@@ -0,0 +1,43 @@
+package week09
+
+import "testing"
+
+func Test_Story_IsValid(t *testing.T) {
+	table := []struct {
+		name string
+		s    *story
+		exp  error
+	}{
+		{
+			name: "publisher invalid",
+			s: &story{
+				// publisher: "Daily Planet",
+				catagory: "spider-man",
+				title:    "Green Goblin Attacks Again",
+				body:     "New York is being terrorised by Green Goblin yet again and spiderman is no where to be found.",
+				writer:   "Eddie Brock",
+			},
+			exp: ErrStoryPublisherInvalid(""),
+		},
+		{
+			name: "publisher invalid",
+			s: &story{
+				publisher: "Daily Planet",
+				// catagory: "spider-man",
+				title:    "Green Goblin Attacks Again",
+				body:     "New York is being terrorised by Green Goblin yet again and spiderman is no where to be found.",
+				writer:   "Eddie Brock",
+			},
+			exp: ErrStoryCatagoryInvalid(""),
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			act := tt.s.IsValid()
+			if act != tt.exp {
+				t.Fatalf("expected %v got %v", tt.exp, act)
+			}
+		})
+	}
+}
